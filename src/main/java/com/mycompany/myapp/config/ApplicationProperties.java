c6b6480bf79a730c2fe7626b6a06f96dd237f4d7
Julien Dubois <julien.dubois@gmail.com>
2019-11-07 14:20:15 +0100

    Add Azure Spring Cloud files with automated GitHub Action deployment

--- src/main/java/com/mycompany/myapp/config/ApplicationProperties.java
+++ src/main/java/com/mycompany/myapp/config/ApplicationProperties.java
@@ -10,4 +10,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
 }
